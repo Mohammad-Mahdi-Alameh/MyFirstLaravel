aafc2bf786dd335e6e3b7f5cdbed5f4f11cb3c1c
Mohammad-Mahdi-Alameh <mmwalameh@gmail.com>
2022-06-06 17:58:08 +0300

    Creating the first api

--- app/Http/Controllers/myController.php
+++ app/Http/Controllers/myController.php
@@ -7,5 +7,23 @@ use Illuminate\Http\Request;
 class myController extends Controller
 {
    
+    function countPalindromeStrings($strings){
+    
+    $count = 0;
+
+    foreach($strings as $string) {
+    
+        if(strrev($string) === $string) {
+    
+            $count++;
+   
+        }
+
+    }
+
+
+    echo $count;
+
+    }
 
 }
